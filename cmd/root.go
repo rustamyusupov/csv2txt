6fe214b490cbc651852d9dcaa77012f773cbaafd
Rustam Yusupov <i@rstm.me>
2025-03-03 18:23:35 +0200

    Add file saving functionality to persist URLs

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/rustamyusupov/csv2txt/internal"
 )
@@ -26,4 +27,11 @@ func Execute() {
 
 	fileData := internal.Parse(records)
 	fmt.Printf("Found %d records with mrs\n", len(fileData))
+
+	folderPath := filepath.Dir(csvFilePath)
+	if err := internal.Save(folderPath, fileData); err != nil {
+		fmt.Printf("Error saving files: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Successfully saved to txt files")
 }
